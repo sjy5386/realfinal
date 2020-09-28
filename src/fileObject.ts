337ed880444ea79677fba2ce70ec2e94ac3e194a
Seokjin Yoon <sjy5386@gmail.com>
2020-09-28 17:37:29 +0900

    Add function readFile and writeFile

--- src/fileObject.ts
+++ src/fileObject.ts
@@ -1,3 +1,5 @@
+import * as fs from 'fs';
+
 class FileObject {
     name: string;
     data: string;
@@ -8,6 +10,19 @@ class FileObject {
     }
 }
 
+function readFile(path: string, filename: string): Promise<FileObject> {
+    return new Promise(async (resolve, reject) => {
+        const buf: Buffer = await fs.promises.readFile(path);
+        resolve(new FileObject(filename, buf));
+    });
+}
+
+function writeFile(path: string, fileObject: FileObject): Promise<void> {
+    return new Promise(async (resolve, reject) => {
+        await fs.promises.writeFile(path, Buffer.from(fileObject.data, 'base64'));
+    });
+}
+
 export {
-    FileObject
+    FileObject, readFile, writeFile
 };
