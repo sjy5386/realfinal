e4f2d235eb138b7155769ab83807a1377e1955bf
Seokjin Yoon <sjy5386@gmail.com>
2020-09-28 15:27:43 +0900

    Add class Commit

--- src/document.ts
+++ src/document.ts
@@ -1,3 +1,23 @@
+import * as crypto from 'crypto';
+
+class Commit {
+    id: string;
+    message: string;
+    author: string;
+    file: FileObject;
+    timestamp: Date;
+    parent: string | undefined;
+
+    constructor(message: string, author: string, file: FileObject, parent: string | null) {
+        this.timestamp = new Date();
+        this.id = crypto.createHash('sha512').update(`${message}${author}${file}${this.timestamp}`).digest('hex');
+        this.message = message;
+        this.author = author;
+        this.file = file;
+        this.parent = parent ? parent : undefined;
+    }
+}
+
 class FileObject {
     name: string;
     data: string;
